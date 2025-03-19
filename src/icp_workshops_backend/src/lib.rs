3d3e61a27094791f11e3e3ffddb699b60db5f21b
Tomasz Kamiński <tomasz.kaminski@zse.krakow.pl>
2025-03-19 09:32:25 +0000

    msg111

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -1,3 +1,20 @@
+use candid::types::number::Nat;
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_name(name: String) {
+    MSG.with(|msg| *msg.borrow_mut() = name);
+}
+
+#[ic_cdk::query]
+fn get_name() -> String{
+    MSG.with(|msg| (*msg.borrow()).clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
